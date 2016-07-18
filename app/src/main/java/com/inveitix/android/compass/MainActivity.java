e56cfe24965f2b371de1ac24a46c6231784af468
Tihomir Krastev <tihomir.krastev@mail.bg>
2016-07-18 14:58:59 +0300

    get magnetic field and accelerometer data to get the phone orientation and shows the degrees in text view, compass image rotate to shows the north

--- app/src/main/java/com/inveitix/android/compass/MainActivity.java
+++ app/src/main/java/com/inveitix/android/compass/MainActivity.java
@@ -3,20 +3,81 @@ package com.inveitix.android.compass;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    @BindView(R.id.txt_heading)
+    TextView txtHeading;
+    @BindView(R.id.img_compass)
+    ImageView imgCompass;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+    private float currentDegree;
+    private float[] gravity;
+    private float[] geomagnetic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        this.accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        this.magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            gravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            geomagnetic = event.values;
+        if (gravity != null && geomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            float degree = 0;
+            boolean success = SensorManager.getRotationMatrix(R, I, gravity, geomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                currentDegree = -orientation[0]*360/(2*3.14159f);
+                Log.e("tag", String.valueOf(currentDegree));
+                degree = Math.round(-orientation[0]*360/(2*3.14159f));
+                Log.e("taag", String.valueOf(degree));
+            }
+            txtHeading.setText("Heading: " + degree + " degrees");
+            RotateAnimation ra = new RotateAnimation( currentDegree, -degree,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            ra.setDuration(210);
+            ra.setFillAfter(true);
+            imgCompass.startAnimation(ra);
+            currentDegree = -degree;
+        }
     }
 
     @Override
