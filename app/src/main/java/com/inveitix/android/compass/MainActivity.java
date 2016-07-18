67ff26aa92f52b0e060e50089d15020235218ac8
Tihomir Krastev <tihomir.krastev@mail.bg>
2016-07-18 18:28:18 +0300

    low-pass filter concept added to decrease sensor sensitivity, landscape layout added, got degrees in different orientations

--- app/src/main/java/com/inveitix/android/compass/MainActivity.java
+++ app/src/main/java/com/inveitix/android/compass/MainActivity.java
@@ -1,12 +1,15 @@
 package com.inveitix.android.compass;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -16,6 +19,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    public static final int REVERSED_PORTRAIT = 180;
+    public static final int LANDSCAPE_DEGREES_DIFFERENCE = 90;
+    public static final float PIVOT_X_VALUE = 0.5f;
+    public static final float PIVOT_Y_VALUE = 0.5f;
+    static final float ALPHA = 0.05f;
+
     @BindView(R.id.txt_heading)
     TextView txtHeading;
     @BindView(R.id.img_compass)
@@ -27,15 +36,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float currentDegree;
     private float[] gravity;
     private float[] geomagnetic;
+    private Display display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        init();
+    }
+
+    private void init() {
         this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         this.accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         this.magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        this.display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
     }
 
     @Override
@@ -54,30 +69,78 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-            gravity = event.values;
+            gravity = lowPassFilterSensitive(event.values.clone(), gravity);
         if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-            geomagnetic = event.values;
+            geomagnetic = lowPassFilterSensitive(event.values.clone(), geomagnetic);
         if (gravity != null && geomagnetic != null) {
             float R[] = new float[9];
             float I[] = new float[9];
             float degree = 0;
             boolean success = SensorManager.getRotationMatrix(R, I, gravity, geomagnetic);
+            float orientation[] = new float[3];
+            SensorManager.getOrientation(R, orientation);
             if (success) {
-                float orientation[] = new float[3];
-                SensorManager.getOrientation(R, orientation);
-                currentDegree = -orientation[0]*360/(2*3.14159f);
-                Log.e("tag", String.valueOf(currentDegree));
-                degree = Math.round(-orientation[0]*360/(2*3.14159f));
-                Log.e("taag", String.valueOf(degree));
+                getDegreesByRotation(orientation);
+                degree = currentDegree;
             }
             txtHeading.setText("Heading: " + degree + " degrees");
-            RotateAnimation ra = new RotateAnimation( currentDegree, -degree,
-                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-            ra.setDuration(210);
-            ra.setFillAfter(true);
-            imgCompass.startAnimation(ra);
+            animateCompass(degree);
             currentDegree = -degree;
         }
+    }
+
+    private void animateCompass(float degree) {
+        RotateAnimation ra = new RotateAnimation(currentDegree, -degree,
+                Animation.RELATIVE_TO_SELF, PIVOT_X_VALUE,
+                Animation.RELATIVE_TO_SELF, PIVOT_Y_VALUE);
+        ra.setDuration(210);
+        ra.setFillAfter(true);
+        imgCompass.startAnimation(ra);
+    }
+
+    private void getDegreesByRotation(float[] orientation) {
+        switch (display.getRotation()) {
+            case Surface.ROTATION_0:
+                getPortraitDegrees(orientation);
+                break;
+            case Surface.ROTATION_90:
+                getLandscapeDegrees(orientation);
+                break;
+            case Surface.ROTATION_180:
+                getReversedPortraitDegrees(orientation);
+                break;
+            case Surface.ROTATION_270:
+                getReversedLandscapeDegrees(orientation);
+                break;
+        }
+    }
+
+    protected float[] lowPassFilterSensitive(float[] input, float[] output ) {
+        if ( output == null ) return input;
+
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
+    }
+
+    private void getPortraitDegrees(float[] orientation) {
+        currentDegree = Math.round(-orientation[0] * 360 / (2 * 3.14159f));
+    }
+
+    private void getReversedLandscapeDegrees(float[] orientation) {
+        currentDegree = Math.round(-orientation[0] * 360 / (2 * 3.14159f)
+                + LANDSCAPE_DEGREES_DIFFERENCE);
+    }
+
+    private void getReversedPortraitDegrees(float[] orientation) {
+        currentDegree = Math.round(-orientation[0] * 360 / (2 * 3.14159f)
+                + REVERSED_PORTRAIT);
+    }
+
+    private void getLandscapeDegrees(float[] orientation) {
+        currentDegree = Math.round(-orientation[0] * 360 / (2 * 3.14159f)
+                - LANDSCAPE_DEGREES_DIFFERENCE);
     }
 
     @Override
